1ab33b6c4b886ca7ff9e8a1445b78da8528ba2db
Nate Hunzaker <nate.hunzaker@gmail.com>
2014-12-09 14:44:41 -0500

    [add] Prettier CSS

--- src/components/ars.js
+++ src/components/ars.js
@@ -4,18 +4,73 @@
  */
 
 var React   = require('react/addons')
+var Photo   = require('../stores/photo')
 var Gallery = require('./gallery')
+var Error   = require('./error')
+
+var Search  = require('./search')
 
 var Ars = React.createClass({
 
+  getInitialState() {
+    return {
+      error  : false,
+      items  : [],
+      search : ''
+    }
+  },
+
+  getDefaultProps() {
+    return {
+      url : 'photos.json',
+    }
+  },
+
+  componentWillMount() {
+    Photo.fetch(this.props.url, this.responseDidSucceed, this.responseDidFail)
+  },
+
+  responseDidSucceed(items) {
+    this.setState({ items, error: false })
+  },
+
+  responseDidFail(error) {
+    this.setState({ error })
+  },
+
   render() {
-    var { src } = this.props
+    var { error, items, search } = this.state
+
+    var filtered = Photo.filter(items, search)
+    var datalist = Photo.datalist(items)
 
     return (
       <div className="ars">
-        <Gallery />
+        <section className="ars-dialog" role="dialog">
+
+          <header className="ars-dialog-header">
+            <h1 className="ars-dialog-title">
+              Please select a photo
+            </h1>
+            <Search key="search" datalist={ datalist } onChange={ this._onSearchChange } />
+          </header>
+
+          <Error key="error" error={ error } />
+
+          <Gallery items={ filtered } />
+
+          <footer className="ars-dialog-footer">
+            <button className="ars-button">Cancel</button>
+            <button className="ars-button">Okay</button>
+          </footer>
+
+        </section>
       </div>
     )
+  },
+
+  _onSearchChange(search) {
+    this.setState({ search, picked: false })
   }
 
 })
--- src/components/gallery.js
+++ src/components/gallery.js
@@ -5,35 +5,20 @@
 
 var React     = require('react/addons')
 var Animation = React.addons.CSSTransitionGroup
-var Photo     = require('../stores/photo')
-var Error     = require('./error')
 var Figure    = require('./figure')
-var Search    = require('./search')
 
 var Gallery = React.createClass({
 
-  getInitialState() {
-    return {
-      items : []
-    }
-  },
-
   getDefaultProps() {
     return {
-      url: 'photos.json'
+      items: []
     }
   },
 
-  componentWillMount() {
-    Photo.fetch(this.props.url, this.responseDidSucceed, this.responseDidFail)
-  },
-
-  responseDidSucceed(items) {
-    this.setState({ items, error: false })
-  },
-
-  responseDidFail(error) {
-    this.setState({ error })
+  getInitialState() {
+    return {
+      picked: false
+    }
   },
 
   getItem(record) {
@@ -46,22 +31,11 @@ var Gallery = React.createClass({
   },
 
   render() {
-    var { error, items, search } = this.state
-
-    var filtered = Photo.filter(items, search)
-    var datalist = Photo.datalist(items)
-
     return (
       <Animation component="div" className="ars-gallery" transitionName="ars-gallery">
-        <Search key="search" datalist={ datalist } onChange={ this._onSearchChange } />
-        <Error key="error" error={ error } />
-        { filtered.map(this.getItem) }
+        { this.props.items.map(this.getItem) }
       </Animation>
     )
-  },
-
-  _onSearchChange(search) {
-    this.setState({ search, picked: false })
   },
 
   _onFigureClick(picked) {
