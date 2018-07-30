446b729c773108c96a08e17bd3dfeef836591371
Nate Hunzaker <nate.hunzaker@gmail.com>
2018-07-30 11:29:32 -0700

    Modernize multiselection.js

--- src/components/multiselection.js
+++ src/components/multiselection.js
@@ -1,60 +1,64 @@
 /**
  * MultiSelection
+ * @flow
  */
 
 import React from 'react'
 import Button from './ui/button'
 import MultiSelectionItem from './multiselection-item'
 import SelectionText from './selection-text'
-import createClass from 'create-react-class'
-import { string, array } from 'prop-types'
 
-let MultiSelection = createClass({
-  propTypes: {
-    resource: string.isRequired,
-    slug: array
-  },
+type Props = {
+  onClick: Event => *,
+  resource: string,
+  slug: string[],
+  url: string
+}
+
+export default class MultiSelection extends React.Component<Props> {
+  hasSlugs() {
+    return this.props.slug && this.props.slug.length
+  }
 
   getItems() {
     let { slug, url } = this.props
-    if (slug && slug.length > 0) {
-      return (
-        <div className="ars-multiselection-grid">
-          {slug.map((s, i) => (
-            <MultiSelectionItem key={i} slug={s} url={url} />
-          ))}
-        </div>
-      )
+
+    if (!this.hasSlugs()) {
+      return null
     }
-  },
+
+    return (
+      <div className="ars-multiselection-grid">
+        {slug.map((slug, key) =>
+          React.createElement(MultiSelectionItem, { url, slug, key })
+        )}
+      </div>
+    )
+  }
 
   render() {
-    let { slug } = this.props
-
     return (
       <div className="ars-multiselection">
         {this.getItems()}
 
         <Button
           ref="button"
-          onClick={this._onClick}
+          onClick={this._onClick.bind(this)}
           className="ars-selection-edit"
         >
           <SelectionText
             resource={this.props.resource}
-            item={slug && slug.length > 0}
+            item={this.hasSlugs()}
             isPlural={true}
           />
           <span className="ars-selection-button-icon" aria-hidden="true" />
         </Button>
       </div>
     )
-  },
-
-  _onClick(e) {
-    e.preventDefault()
-    this.props.onClick(e)
   }
-})
 
-export default MultiSelection
+  _onClick(event: MouseEvent) {
+    event.preventDefault()
+    this.props.onClick(event)
+  }
+}
