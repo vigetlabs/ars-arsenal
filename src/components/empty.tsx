36fe017c8a1cf4219c254b22347ace039d48c93c
Nate Hunzaker <nate.hunzaker@gmail.com>
2018-10-16 12:51:00 -0700

    Break out empty component

---
+++ src/components/empty.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+interface Props {
+  fetching: boolean
+  search: string
+}
+
+const Empty: React.SFC<Props> = ({ fetching, search }) => {
+  if (fetching) {
+    return <p className="ars-empty">Awaiting data...</p>
+  }
+  return (
+    <p className="ars-empty">
+      No items exist {search ? `for “${search}”.` : ''}
+    </p>
+  )
+}
+
+export default Empty
